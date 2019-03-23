e366f7560412431e67fe7c32ce3197cd5802c734
mucheniski <mucheniski@gmail.com>
2019-03-23 07:47:26 -0300

    5.6 Regra para nao salvar pessoa inativa ou inexistente

--- src/main/java/com/example/algamoney/api/exceptionhandler/AlgamoneyExceptionHandler.java
+++ src/main/java/com/example/algamoney/api/exceptionhandler/AlgamoneyExceptionHandler.java
@@ -55,7 +55,7 @@ public class AlgamoneyExceptionHandler extends ResponseEntityExceptionHandler{
 	
 	@ExceptionHandler({ DataIntegrityViolationException.class })
 	private ResponseEntity<Object> handleDataIntegrityViolationException(DataIntegrityViolationException ex, WebRequest request){
-		String mensagemUsuario = messageSource.getMessage("recurso.nao-encontrado", null, LocaleContextHolder.getLocale());
+		String mensagemUsuario = messageSource.getMessage("recurso.operacao-nao-permitida", null, LocaleContextHolder.getLocale());
 		String mensagemDesenvolvedor = ExceptionUtils.getRootCauseMessage(ex);
 		List<Erro> erros = Arrays.asList(new Erro(mensagemUsuario, mensagemDesenvolvedor));
 		return handleExceptionInternal(ex, erros, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
