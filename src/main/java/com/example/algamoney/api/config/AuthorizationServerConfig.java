8763efdc09feae2885fc562129371325e43d8c26
mucheniski <mucheniski@gmail.com>
2019-04-20 16:33:50 -0300

    Ajuste tempo do token

--- src/main/java/com/example/algamoney/api/config/AuthorizationServerConfig.java
+++ src/main/java/com/example/algamoney/api/config/AuthorizationServerConfig.java
@@ -35,7 +35,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 			.secret("@ngul@r0")
 			.scopes("read", "write")
 			.authorizedGrantTypes("password", "refresh_token")
-			.accessTokenValiditySeconds(300)
+			.accessTokenValiditySeconds(1800)
 			.refreshTokenValiditySeconds(3600 * 24)
 		.and()
 			.withClient("mobile")
