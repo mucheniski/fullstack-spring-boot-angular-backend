2f74fa8b698140392c6c78990ecefab0b71c52ea
mucheniski <mucheniski@gmail.com>
2019-05-12 19:12:57 -0300

    Fix pesquisa de cidade por estado

--- src/main/java/com/example/algamoney/api/resource/CidadeResource.java
+++ src/main/java/com/example/algamoney/api/resource/CidadeResource.java
@@ -3,10 +3,10 @@ package com.example.algamoney.api.resource;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.algamoney.api.model.Cidade;
@@ -19,20 +19,10 @@ public class CidadeResource {
 	@Autowired
 	private CidadeRepository cidadeRepository;
 	
-	@GetMapping	
-	public List<Cidade> listar() {
-		return cidadeRepository.findAll();		
-	}
-	
-	@GetMapping("/{codigo}")	
-	public ResponseEntity<Cidade> buscarPeloCodigo(@PathVariable Long codigo) {
-		Cidade cidade = cidadeRepository.findOne(codigo);
-		return cidade != null ? ResponseEntity.ok().body(cidade) : ResponseEntity.notFound().build(); 
-	}
-	
-	@GetMapping("/estado/{codigo}")	
-	public List<Cidade> buscarPorEstado(@PathVariable Long codigo) {
-		return cidadeRepository.findByEstadoCodigo(codigo);		 
+	@GetMapping
+	@PreAuthorize("isAuthenticated()")
+	public List<Cidade> pesquisar(@RequestParam Long estado) {
+		return cidadeRepository.findByEstadoCodigo(estado);
 	}
 
 
